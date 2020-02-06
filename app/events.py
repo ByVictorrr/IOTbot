2199130b5fd744cff281c85918dd004bf214a46b
Victor Delaplaine <vdelaplaines@aol.com>
2020-02-05 16:22:02 -0800

    got the client/robot room confg

--- app/events.py
+++ app/events.py
@@ -1,38 +1,28 @@
 from app import sio
 from app.models import User
 import pdb
-
-lookup_bot = dict()
-
-"""
-@sio.on('connect')
-def connect():
-    print("connecting!")
-    sio.emit('connect','authorized complete')
-    """
+from flask_socketio import join_room
+# [key is username] - > value: room name
+lookup_room = dict()
 
 
 @sio.on("message")
 def handle_message(msg):
-    pass
-    #breakpoint()
-
-
-#For robot interaction
-# data = "{username, sid}"
-@sio.on("auth")
-def set_up_bot(data):
-    #breakpoint()
-    add_to_lookup_bot(data['sid'],data['username'])
+    breakpoint()
+    sio.emit('message',msg[0],room=lookup_room[msg[1]])
 
 # this get the sid sent back to client
 @sio.on("connect bot")
 def connect_bot(user):
-    #breakpoint()
     sio.emit("connect bot", lookup_bot[user])
 
 
-
+#For robot interaction
+# data = "{username, sid}"
+@sio.on("setup room")
+def set_up_bot(data):
+    add_to_lookup_room(data['sid'],data['username'])
+    
 
 """
 Description: a way for the arduino to give acess to user to it using sid
@@ -40,11 +30,11 @@ Description: a way for the arduino to give acess to user to it using sid
     Given Cred(username, pasword) and sid(of the bot),
         add to the dictionary st: lookup_bot[user] = sid of bot
 """
-def add_to_lookup_bot(sid, username):
+def add_to_lookup_room(sid, username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         emit("message","No valid username found" ,room=sid)
     else:
-        # add to the dictonary(object = User: "sid")
-        lookup_bot[user.username] = sid
+        lookup_room[user.username] = sid 
+        join_room(sid)
 
--- robotTest/app.py
+++ robotTest/app.py
@@ -4,15 +4,16 @@ sio=Client()
 sio.connect('http://localhost:5000')
 
 username = "victor"
+password = "victor1"
 
 @sio.event
 def connect():
 	breakpoint()
-	sio.emit("auth", data={'username': username, 'sid': sio.sid})
+	sio.emit("setup room", data={'username': username, 'sid': sio.sid})
+
 
 @sio.on('message')
 def on_message(msg):
-	breakpoint()
 	print(msg)
 
 
